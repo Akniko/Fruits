6f1e257bf12d29e3ba1f2141c863719c1f68b9fb
Akniko <akniyet.rahmanbekovna@gmail.com>
2023-08-10 10:34:00 +0600

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Akniyet Alibek on 10.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
